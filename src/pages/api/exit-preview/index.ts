96991eb73c9e4cb18430388899d73416a9314c7b
Lucas Juste <lucasjuste@Lucass-MacBook-Pro.local>
2021-10-14 12:25:57 -0300

    ready

--- src/pages/api/exit-preview/index.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-export default async (_, res) => {
-    res.clearPreviewData();
-  
-    res.writeHead(307, { Location: "/" });
-    res.end();
-  };
